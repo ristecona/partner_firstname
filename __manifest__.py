5f2722c8d2e14361fac87df9317b13434d70460e
Riste Cona <ristecona@gmail.com>
2017-04-24 16:10:26 +0200

    Added toggle firstname lastname

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2013 Nicolas Bessi (Camptocamp SA)
+# © 2014 Agile Business Group (<http://www.agilebg.com>)
+# © 2015 Grupo ESOC (<http://www.grupoesoc.es>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Partner first name and last name',
+    'summary': "Split first name and last name for non company partners",
+    'version': '10.0.1.0.0',
+    'author': "CONA, "
+              "Grupo ESOC Ingeniería de Servicios, "
+              "Odoo Community Association (OCA)",
+    'license': "AGPL-3",
+    'maintainer': 'Camptocamp, Acsone',
+    'category': 'Extra Tools',
+    'website':
+        'http://www.camptocamp.com, http://www.acsone.eu, http://grupoesoc.es',
+    'depends': ['base'],
+    'data': [
+        'views/res_partner.xml',
+        'views/res_user.xml',
+        'data/res_partner.yml',
+    ],
+    'auto_install': False,
+    'installable': True,
+}
